0a71e46b29b3ff3578d620db2ef17b0749b96a15
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-08-19 22:06:02 +0530

    Added componentDidMount() to get geolocation

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 class App extends React.Component {
-
     constructor(props) {
         // Throws an error if there is no super(props) call. Must be defined as the first line.
         super(props);
-        this.state = { lat: null, errorMessage: ''};
+        this.state = { lat: null, long: null, errorMessage: "" };
+    }
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-                },
-            (error) => {
-                this.setState({ errorMessage: error.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (error) => this.setState({ errorMessage: error.message })
         );
-    };
+    }
 
-    // React says me must compulsorily have a render method.
+    // React says me must compulsorily have a render method. And it will run multiple times. it should only be used for returning JSX.
     render() {
-        
-        if(!this.state.lat && this.state.errorMessage) {
+        if (!this.state.lat && this.state.errorMessage) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
-        if(!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Longitude: {this.state.long}</div>;
         }
-        return <div>Loading...</div>;    
-    };
+        return <div>Loading...</div>;
+    }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
